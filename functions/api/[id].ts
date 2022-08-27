5a7bcf7dbb2d73ef6ad9c2cb14e52da055645015
idnewnil <13927553153@163.com>
2022-08-27 22:49:20 +0800

    恢复

--- functions/api/[id].ts
+++ functions/api/[id].ts
@@ -6,15 +6,19 @@ interface Data {}
 
 type Context = EventContext<Env, "id", Data>;
 
-export async function onRequestGet({ env, params }: Context) {
-  let id = params.id as string;
-  let value = await env.PORTAL.get(id);
-  return new Response(value);
+export async function onRequestGet() {
+  return new Response("Hello, world!");
 }
 
-export async function onRequestPost({ request, env, params }: Context) {
-  let url = new URL(request.url);
-  let id = params.id as string;
-  await env.PORTAL.put(id, url.searchParams.get("value"));
-  return new Response("ok");
-}
+// export async function onRequestGet({ env, params }: Context) {
+//   let id = params.id as string;
+//   let value = await env.PORTAL.get(id);
+//   return new Response(value);
+// }
+
+// export async function onRequestPost({ request, env, params }: Context) {
+//   let url = new URL(request.url);
+//   let id = params.id as string;
+//   await env.PORTAL.put(id, url.searchParams.get("value"));
+//   return new Response("ok");
+// }
